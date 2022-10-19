5789bf4f86509899803b94bca5d068b15bf45a51
fuyqz <112553934+fuyqz@users.noreply.github.com>
2022-10-19 12:04:50 +0800

    Add files via upload

---
+++ 留言板小程序/pages/index/index.vue
@@ -0,0 +1,109 @@
+<template>
+    <view class="content">
+        <input type="text" class="input" v-model="content" placeholder="留言">
+        <button type="primary" @click="publish()" size="mini">发布</button>
+        <button type="primary" @click="getList('getMyMessages')" size="mini">我的留言</button>
+    </view>
+    <view v-for="item in list" :key="item_id">
+        <view class="box">
+            <view>{{item.content }}</view>
+            <view v-if="!item.publish">未公开</view>
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                content: '',
+                list: [],
+            }
+        },
+        onLoad() {
+            //获取token
+            let token = uni.getStorageSync('token');
+            //不存在token,使用第三方程序获取第三方code
+            if (!token) {
+                //调用loginWithMp函数
+                uni.login().then(({
+                    code
+                }) => {
+                    uniCloud.callFunction({
+                        name: 'fun',
+                        data: {
+                            api: 'loginWithMp',
+                            code
+                        }
+                    }).then(({
+                        result
+                    }) => {
+                        token = result.token;
+                        //保存到前端
+                        uni.setStorageSync('token', token)
+                        //列表查询
+                        this.getList();
+                    })
+                })
+            } else {
+                //已登录,直接查询
+                this.getList();
+            }
+        },
+        methods: {
+            getList(api) {
+                uniCloud.callFunction({
+                    name: 'fun',
+                    data: {
+                        api: api || 'getMessages',
+                        token: uni.getStorageSync('token')
+                    }
+                }).then(res => {
+                    this.list = res.result.data
+                })
+            },
+            publish() {
+                uniCloud.callFunction({
+                    name: 'fun',
+                    data: {
+                        api: 'publish',
+                        content: this.content,
+                        token: uni.getStorageSync('token')
+                    }
+                }).then(res => {
+                    this.content = '';
+                    uni.showToast({
+                        title: '留言成功',
+                        icon: 'success'
+                    })
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    .content {
+        display: flex;
+        align-items: center;
+        margin: 40px;
+
+    }
+
+    .input {
+        border-bottom: 1px solid #ccc;
+        padding: 6px;
+        flex: 1;
+    }
+
+    .box {
+        margin: 40rpx;
+        border-bottom: 1px solid #ccc;
+        padding: 20rpx 0;
+
+    }
+</style>
+
+
+
+}
---
+++ 留言板小程序/uniCloud-aliyun/cloudfunctions/fun/index.js
@@ -0,0 +1,87 @@
+//引入第三方库
+let jwt = require('jsonwebtoken')
+let db = uniCloud.database({
+    throwOnNotFount: false,
+})
+//appId 和 appSecret必须填你自己的,不然会报错  后台修改request合法域名和uploadFile合法域名
+//修改request文档地址 https://uniapp.dcloud.net.cn/uniCloud/publish.html
+//小程序后台地址 https://mp.weixin.qq.com 
+let appId = '填你自己的'
+let appSecret = '填你自己的'
+let jwtSecret = 'onejwtSecret'
+
+exports.main = async (event, context) => {
+    if (event.api == 'loginWithMp') {
+        //发送请求 uniCloud.httpclient.request(URL,requestOptions)
+        let wxRes = await uniCloud.httpclient.request(
+            //注意是键盘左上角 ``
+            `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${event.code}&grant_type=authorization_code`, {
+                dataType: "json",
+            });
+
+        let userId = wxRes.data.openid;
+        if (!userId) {
+            throw Error(wxRes.data.errmsg)
+        }
+        let user = await db.collection('user').doc('userId').get();
+        //声明token变量 固定格式'Bearer '+
+        let token = 'Bearer ' + jwt.sign({
+            userId
+        }, jwtSecret)
+        if (user.data[0]) {
+            //存在则直接返回
+            return {
+                user: user.data[0],
+                token
+            }
+        } else {
+            //不存在,新增一个用户数据并返回
+            let data = {
+                _id: userId,
+                createdAt: Date.now()
+            }
+            await db.collection('user').add(data);
+            return {
+                user: data,
+                token
+            }
+        }
+    }
+    if (!event.token) {
+        throw Error('未登录')
+    }
+    //使用verify验证并返回函数
+    let auth = jwt.verify(event.token.replace('Bearer ', ''), jwtSecret)
+    let userId = auth.userId;
+    if (event.api == 'publish') {
+        return await db.collection('message').add({
+            content: event.content,
+            public: false,
+            userId,
+        })
+    }
+    if (event.api == 'getMessages') {
+        return await db.collection('message').where({
+            public: true
+        }).get()
+    }
+    if (event.api == 'getMyMessages') {
+        return await db.collection('message').where({
+            userId
+        }).get()
+    }
+    if (event.api == 'deleteMyMessage') {
+        return await db.collection('message').where({
+            userId
+        }).get()
+    }
+
+
+
+    //返回数据给客户端
+    return ''
+};
+
+//  cd .\uniCloud-aliyun\cloudfunctions\fun 进入函数目录
+// npx nrm use taobao  切换下载镜像为国内
+// npm install jsonwebtoken 下载解密token的第三方库
